34cdf9a96ba4e1d32e140fb4fe180f443b7ff677
Oliver Chambers <o.chambers2@gmail.com>
2020-03-05 16:16:37 +0100

    fix up pyenv installer
    add apt dependencies for python and some pretty_print-ing

--- pyenv/install.sh
+++ pyenv/install.sh
@@ -11,19 +11,22 @@ set -o pipefail         # Use last non-zero exit code in a pipeline
 # DESC: Parameter parser
 # ARGS: $@ (optional): Arguments provided to the script
 # OUTS: Variables indicating command-line parameters and options
-function parse_params() 
+function parse_params()
 {
     local param
+    virtualenv=false
+
     while [[ $# -gt 0 ]]; do
         param="$1"
         shift
         case $param in
             --version)
+                version="$1"
                 shift
-                version=$1
                 ;;
             --virtualenv)
                 virtualenv=true
+                ;;
             *)
                 script_exit "Invalid parameter was provided: $param" 1
                 ;;
@@ -33,22 +36,34 @@ function parse_params()
 
 
 function main()
-{   
+{
+    source "$HOME/dotfiles/scripts/utils.sh"
+    colour_init
+
+    local style="$fg_cyan$ta_bold"
+
     parse_params "$@"
-   
-    # clone the repo
+
+    pretty_print "install python dependencies..." "$style"
+    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+        libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+        libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev \
+        liblzma-dev python-openssl git
+
+    pretty_print "cloning pyenv from github" "$style"
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-    
-    # if a version is given install it
-    if [ -n "$version" ]; then
-        PYENV_ROOT="$HOME/.pyenv
-        PATH="$PYENV_ROOT/bin:$PATH
+
+    if [ -n "${version-}" ]; then
+        pretty_print "installing python version $version" "$style"
+        export PYENV_ROOT="$HOME/.pyenv"
+        export PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init -)"
-        
+
         pyenv install "$version"
     fi
 
-    if virtualenv; then
+    if $virtualenv; then
+        pretty_print "installing pyenv-virtualenv plugin" "$style"
         plugin_dir="$HOME/.pyenv/plugins/pyenv-virtualenv"
         git clone https://github.com/pyenv/pyenv-virtualenv.git "$plugin_dir"
     fi
