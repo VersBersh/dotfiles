9e3821fd984be1d5e3cb4b7a9739102892da58eb
Oliver Chambers <o.chambers2@gmail.com>
2020-03-05 17:16:19 +0100

    update pyenv installer
    - more pretty_printing
    - check if ~/.pyenv directory already exists
    - unset parameters after use

--- pyenv/install.sh
+++ pyenv/install.sh
@@ -14,6 +14,7 @@ set -o pipefail         # Use last non-zero exit code in a pipeline
 function parse_params()
 {
     local param
+    version=''
     virtualenv=false
 
     while [[ $# -gt 0 ]]; do
@@ -40,35 +41,41 @@ function main()
     source "$HOME/dotfiles/scripts/utils.sh"
     colour_init
 
-    local style="$fg_cyan$ta_bold"
+    local info_style="$fg_cyan$ta_bold"
+    local warn_style="$fg_yellow$ta_emph"
 
     parse_params "$@"
 
-    pretty_print "install python dependencies..." "$style"
-    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-        libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-        libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev \
-        liblzma-dev python-openssl git
+    export PYENV_ROOT="$HOME/.pyenv"
+    if [[ -d "$PYENV_ROOT" ]]; then
+        pretty_print "pyenv already installed" "$warn_style"
+    else
+        pretty_print "install python dependencies..." "$info_style"
+        sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+            libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+            libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev \
+            liblzma-dev python-openssl git
 
-    pretty_print "cloning pyenv from github" "$style"
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+        pretty_print "cloning pyenv from github" "$info_style"
+        git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    fi
 
-    if [ -n "${version-}" ]; then
-        pretty_print "installing python version $version" "$style"
-        export PYENV_ROOT="$HOME/.pyenv"
+    if [ -n "$version" ]; then
+        pretty_print "installing python version $version" "$info_style"
         export PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init -)"
 
         pyenv install "$version"
     fi
+    unset version
 
     if $virtualenv; then
-        pretty_print "installing pyenv-virtualenv plugin" "$style"
+        pretty_print "installing pyenv-virtualenv plugin" "$info_style"
         plugin_dir="$HOME/.pyenv/plugins/pyenv-virtualenv"
         git clone https://github.com/pyenv/pyenv-virtualenv.git "$plugin_dir"
     fi
+    unset virtualenv
 }
 
 
 main "$@"
-
